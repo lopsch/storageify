f1e3ee6bd3e52514f237724802e6dfebe63a53bc
Daniel Lopes <lopsch@googlemail.com>
2017-08-11 16:32:16 +0200

    refactoring

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -2,25 +2,18 @@ import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
 import progress from 'rollup-plugin-progress'
 import eslint from 'rollup-plugin-eslint'
-
-const babelConfig = {
-  babelrc: false,
-  exclude: 'node_modules/**',
-  presets: ['es2015-rollup'],
-  plugins: ['transform-object-rest-spread'],
-  env: { coverage: { plugins: ['istanbul'] } }
-}
+import rolluprc from './.rolluprc.json'
 
 export default [
   {
-    entry: 'src/index.js',
+    entry: rolluprc.entryConfig,
     external: ['xstream'],
     dest: pkg.module,
     format: 'es',
     plugins: [
       progress({ clearLine: false }),
       eslint({ throwOnError: true, throwOnWarning: true }),
-      babel(babelConfig)
+      babel(rolluprc.babelConfig)
     ],
     sourceMap: false
   }
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -4,20 +4,11 @@ import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
 import progress from 'rollup-plugin-progress'
 import visualizer from 'rollup-plugin-visualizer'
-
-const isCov = process.env.BABEL_ENV === 'coverage'
-
-const babelConfig = {
-  babelrc: false,
-  exclude: 'node_modules/**',
-  presets: ['es2015-rollup'],
-  plugins: ['transform-object-rest-spread'],
-  env: { coverage: { plugins: ['istanbul'] } }
-}
+import rolluprc from './.rolluprc.json'
 
 export default [
   {
-    entry: 'src/index.js',
+    entry: rolluprc.entryConfig,
     dest: pkg.browser,
     format: 'umd',
     moduleName: 'storageify',
@@ -25,33 +16,19 @@ export default [
       progress({ clearLine: false }),
       resolve(),
       commonjs(),
-      babel(babelConfig),
-      !isCov && visualizer({ filename: './stats/stats.umd.html' })
+      babel(rolluprc.babelConfig),
+      visualizer({ filename: './stats/index.html' })
     ],
     sourceMap: true
   },
   {
-    entry: 'src/index.js',
+    entry: rolluprc.entryConfig,
     external: ['xstream'],
-    dest: pkg.module,
-    format: 'es',
-    plugins: [
-      progress({ clearLine: false }),
-      babel(babelConfig),
-      !isCov && visualizer({ filename: './stats/stats.esm.html' })
+    targets: [
+      { dest: pkg.module, format: 'es' },
+      { dest: pkg.main, format: 'cjs' }
     ],
-    sourceMap: true
-  },
-  {
-    entry: 'src/index.js',
-    external: ['xstream'],
-    dest: pkg.main,
-    format: 'cjs',
-    plugins: [
-      progress({ clearLine: false }),
-      babel(babelConfig),
-      !isCov && visualizer({ filename: './stats/stats.cjs.html' })
-    ],
+    plugins: [progress({ clearLine: false }), babel(rolluprc.babelConfig)],
     sourceMap: true
   }
 ]
