c9427502d951be6c69525783c524584086f7a826
Daniel Lopes <lopsch@googlemail.com>
2017-08-18 16:59:09 +0200

    move to rollup minify plugin

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,5 +1,6 @@
 import pkg from './package.json'
 import babel from 'rollup-plugin-babel'
+import minify from 'rollup-plugin-babel-minify'
 import progress from 'rollup-plugin-progress'
 import rolluprc from './.rolluprc.json'
 
@@ -10,7 +11,11 @@ export default [
       { dest: pkg.module, format: 'es' },
       { dest: pkg.main, format: 'cjs' }
     ],
-    plugins: [progress({ clearLine: false }), babel(rolluprc.babelConfig)],
+    plugins: [
+      progress({ clearLine: false }),
+      babel(rolluprc.babelConfig),
+      minify()
+    ],
     sourceMap: true,
     external: id => rolluprc.externalConfig.includes(id)
   }
